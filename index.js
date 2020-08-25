b558d12495e8f75910902bc9317b9d373709c4e9
aliyandev <70102220+aliyandev@users.noreply.github.com>
2020-08-25 19:24:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -735,6 +735,71 @@ client.sendMessage(media);
    
   })();
  }
+ else if (msg.body.startsWith("!over ")) {
+	 msg.reply("_Sedang di proses_ *NO SPAM*")
+	  var h = msg.body.split("!over ")[1];
+	 var nama = h.split("] ")[1];
+	 var kata1 = h.split("[")[1].split("]")[0];
+	 	const { exec } = require("child_process");
+
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+
+    });
+    const page = await browser.newPage();
+    await page
+      .goto("https://en.ephoto360.com/create-youtube-banner-game-overwatch-409.html", {
+        waitUntil: "networkidle2",
+      })
+      .then(async () => {
+		  		await page.click("#radio0-radio-sapriyt1g");
+     await page.type("#text-1", kata1);
+   
+		  await page.type("#text-0", nama);
+		await page.click("#submit");
+		await new Promise(resolve => setTimeout(resolve, 10000));
+        try {
+         
+          await page.waitForSelector(
+            "#link-image"
+          );
+          const element = await page.$(
+         "div.thumbnail > img"
+          );
+          const text = await (await element.getProperty("src")).jsonValue();
+         console.log(text);
+
+        exec('wget "' + text + '" -O mp4/ff.jpg', (error, stdout, stderr) => {
+  const media = MessageMedia.fromFilePath('mp4/ff.jpg');
+
+	chat.sendMessage(media);
+	if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+});
+          browser.close();
+        } catch (error) {
+          console.log(error);
+       
+
+        }
+      })
+      .catch((err) => {
+        console.log(error);
+    
+      });
+	 
+	 
+  })();
+ }
 
    else if (msg.body.startsWith("!goldplay ")) {
    msg.reply("_Mohon Menunggu Juragan_")
@@ -2578,6 +2643,8 @@ Dibuat Oleh : *Alif Putra Darmawan*
 • *!goldplay* Namamu
 • *!arum* Namamu
 • *!prepayer* [TEXT1] [TEXT2]
+• *!lolmaker* [AZ WhatsApp Bot] [NAMAMU]
+• *!over* [AZ WhatsApp Bot] [NAMAMU]
 • *!spop* Namamu
 
 ✨ Note : Dilarang Menggunakan Karakter Emoticon Atau Symbol Dalam Pembuatan Logo Maker Bot Ini.
